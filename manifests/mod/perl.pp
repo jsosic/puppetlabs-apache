880889843692a8db8684d1b0f8cf86c33d7120ae
Jakov Sosic <jsosic@gmail.com>
2018-02-13 01:53:14 +0100

    Add perl.conf template

--- manifests/mod/perl.pp
+++ manifests/mod/perl.pp
@@ -1,4 +1,14 @@
 class apache::mod::perl {
   include ::apache
   ::apache::mod { 'perl': }
+  # Template uses no variables
+  file { 'perl.conf':
+    ensure  => file,
+    mode    => $::apache::file_mode,
+    path    => "${::apache::mod_dir}/perl.conf",
+    content => template('apache/mod/perl.conf.erb'),
+    require => Exec["mkdir ${::apache::mod_dir}"],
+    before  => File[$::apache::mod_dir],
+    notify  => Class['apache::service'],
+  }
 }
